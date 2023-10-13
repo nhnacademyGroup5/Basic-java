65a71b95df3f1318ba183a07c1b47d7c03cf2fc5
jiwonkim <kjw3568@gmail.com>
2023-10-13 16:33:33 +0900

    #20231013 jiwon upload chapter 2

---
+++ BasicJava/src/main/java/com/nhnacademy/groupstudy/chapter2/jiwon/Exercise2_1.java
@@ -0,0 +1,17 @@
+package com.nhnacademy.groupstudy.chapter2.jiwon;
+
+public class Exercise2_1 {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("           ******           *************        **********");
+        System.out.println("           **    **                **            **");
+        System.out.println("           **     **               **            **");
+        System.out.println("           **      **              **            **");
+        System.out.println("           **      **              **            ********");
+        System.out.println("           **      **       **     **            **");
+        System.out.println("           **     **         **    **            **");
+        System.out.println("           **    **           **  **             **");
+        System.out.println("           *****               ****              **********");
+        System.out.println();
+    }
+}
---
+++ BasicJava/src/main/java/com/nhnacademy/groupstudy/chapter2/jiwon/Exercise2_3.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.groupstudy.chapter2.jiwon;
+
+import java.util.Scanner;
+
+public class Exercise2_3 {
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println(name.toUpperCase());
+    }
+}
